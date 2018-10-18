7325ffebeef36053011de1a3a905af90275cf863
robert-friedland <43350386+robert-friedland@users.noreply.github.com>
2018-10-18 08:14:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ app.post('/', (req, res, next) => {
     token: process.env.SLACK_TOKEN,
     profile: JSON.stringify({
       "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`,
-      "status_emoji": ":telephone_receiver:",
-      "status_expiration": new Date(end).getTime()/1000
+      "status_emoji": ":telephone_receiver:"
+//       "status_expiration": bigint(end)/1000
     })
   });
   res.status(200);
 //   res.send('🤘');
-  res.send(new Date(end).getTime()/1000);
+  res.send(end/1000);
 });
 
 app.get('/', (req, res, next) => {
