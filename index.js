03c4881588dce76efa6f463fbd28ae07e47b30d3
robert-friedland <43350386+robert-friedland@users.noreply.github.com>
2018-10-18 08:06:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/', (req, res, next) => {
     token: process.env.SLACK_TOKEN,
     profile: JSON.stringify({
       "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`,
-      "status_emoji": ":telephone_receiver:"
+      "status_emoji": ":telephone_receiver:",
       "status_expiration": new Date(end).getTime()
     })
   });
